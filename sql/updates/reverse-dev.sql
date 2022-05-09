3c8bce8876226cb7aacda220ea74938d47804312
Markos Fountoulakis <markos@timescale.com>
2022-05-09 14:55:07 +0300

    Fix downgrade scripts

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -5,3 +5,7 @@ RETURNS INTEGER AS '@MODULE_PATHNAME@', 'ts_policy_retention_add' LANGUAGE C VOL
 DROP FUNCTION IF EXISTS  @extschema@.add_compression_policy(REGCLASS, "any", BOOL, INTERVAL);
 CREATE FUNCTION @extschema@.add_compression_policy(hypertable REGCLASS, compress_after "any", if_not_exists BOOL = false)
 RETURNS INTEGER AS '@MODULE_PATHNAME@', 'ts_policy_compression_add' LANGUAGE C VOLATILE STRICT;
+DROP FUNCTION IF EXISTS @extschema@.add_policies;
+DROP FUNCTION IF EXISTS @extschema@.remove_policies;
+DROP FUNCTION IF EXISTS @extschema@.alter_policies;
+DROP FUNCTION IF EXISTS @extschema@.show_policies;
